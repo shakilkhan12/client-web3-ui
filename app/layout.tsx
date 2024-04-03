ca74489c48a8dee43bb98d7ecec5d69bb2cd49ac
shakilkhan12 <shakilkhan621@gmail.com>
2024-04-03 05:35:01 +0500

    background image updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,22 +24,22 @@ export default function RootLayout({
         <ReduxProvider>
         <Connection>
       <Nav />
-      <div className="absolute left-0 top-0 w-full xl:w-[1170px] h-[645px] -z-[999]">
+      <div className="absolute left-0 top-0 w-full h-full -z-[999]">
         <Image
-          src="/images/blueLine.png"
+          src="/images/bg.png"
           fill
-          className="w-full h-full object-contain"
+          className="w-full h-full object-cover"
           alt="line"
         />
       </div>
-      <div className="absolute right-0 bottom-0 w-full xl:w-[1170px] h-[645px] -z-[999]">
+      {/* <div className="absolute right-0 bottom-0 w-full xl:w-[1170px] h-[645px] -z-[999]">
         <Image
           src="/images/blueLine.png"
           fill
           className="w-full h-full object-contain rotate-180"
           alt="line"
         />
-      </div>
+      </div> */}
         <div className="max-w-[1240px] w-full px-2 py-14 mx-auto min-h-[calc(100vh-100px)] flex items-center justify-center">{children}</div></Connection></ReduxProvider></body>
     </html>
   );
